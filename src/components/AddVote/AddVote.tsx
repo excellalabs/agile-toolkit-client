d9c08143024e88509b5e866cebeb57b5dad715a3
Alex Hoffman <alexhoffman617@gmail.com>
2019-03-11 18:38:39 -0400

    Added in add-votes page, current adds votes to session but does not update on screen

---
+++ src/components/AddVote/AddVote.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react'
+
+interface Props {
+  //classes: Classes
+  addVote: Function
+}
+
+interface Classes {
+  fab: string
+}
+
+class AddVote extends React.Component<Props> {
+  updateVoteValue(event) {
+    this.voteValue = event.target.value
+  }
+
+  addVote() {
+    this.props.addVote(this.voteValue)
+  }
+  voteValue
+
+  public render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.voteValue}
+          onChange={event => this.updateVoteValue(event)}
+        />
+        {/* <button onClick={() => this.addVote()} disabled={this.state.hasVoted}>Add Vote</button> */}
+        <button onClick={() => this.addVote()}>Add Vote</button>
+      </div>
+    )
+  }
+}
+
+export default AddVote
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react'
 import Cards from '../../components/Cards'
+import AddVote from '../../components/AddVote'
 import FlexView from 'react-flexview'
+import { func } from 'prop-types'
 
 const session = {
   flipped: true,
   votes: [{ value: '1', voter: 'Test' }, { value: '2', voter: 'Test2' }]
+}
+
+const addVote = function(value) {
+  session.votes.push({ value: value, voter: 'Test3' })
 }
 
 const Home = () => (
@@ -13,6 +19,9 @@ const Home = () => (
       <h1>Agile Toolkit</h1>
       <FlexView wrap={true} hAlignContent="center">
         <Cards session={session} />
+      </FlexView>
+      <FlexView wrap={true} hAlignContent="center">
+        <AddVote addVote={addVote} />
       </FlexView>
     </div>
   </div>
