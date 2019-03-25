c8442f8eae223fa0df1193337a7e9bdac33b2b9c
Cameron Ivey <cameron.ivey@excella.com>
2019-03-25 19:59:32 -0400

    Connect to server - get/add votes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import * as React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-
 import { Home, PlanningPoker } from './pages'
 import { routes } from './routes'
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+
+const client = new ApolloClient({
+    uri: "http://localhost:4000"
+});
 
 class App extends React.Component {
   public render() {
     return (
-      <Router>
-        <div>
-          <Route path={routes.home} exact={true} component={Home} />
-          <Route path={routes.planningPoker} component={PlanningPoker} />
-        </div>
-      </Router>
+      <ApolloProvider client={client}>
+        <Router>
+          <div>
+            <Route path={routes.home} exact={true} component={Home} />
+            <Route path={routes.planningPoker} component={PlanningPoker} />
+          </div>
+        </Router>
+      </ApolloProvider>
     )
   }
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,30 +1,17 @@
 import * as React from 'react'
 import Cards from '../../components/Cards'
-import AddVote from '../../components/AddVote'
 import FlexView from 'react-flexview'
 import { Session } from '../../models/session'
 import CardFlipper from '../../components/CardFlipper'
-const session = {
-  flipped: false,
-  votes: [{ value: '1', voter: 'Test' }, { value: '2', voter: 'Test2' }]
-}
+import AddVote from '../../components/AddVote'
 
 class Home extends React.Component<{}, Session> {
   constructor(props) {
     super(props)
-    this.state = session
-  }
-
-  addVote(value) {
-    this.setState({
-      votes: [
-        ...this.state.votes,
-        { value: value, voter: 'Test' + (this.state.votes.length + 1) }
-      ]
-    })
   }
 
   flipCards() {
+    // This doesn't do anything now
     this.setState({ flipped: true })
   }
 
@@ -34,10 +21,10 @@ class Home extends React.Component<{}, Session> {
         <div>
           <h1>Agile Toolkit</h1>
           <FlexView wrap={true} hAlignContent="center">
-            <Cards session={this.state} />
+            <Cards />
           </FlexView>
           <FlexView wrap={true} hAlignContent="center">
-            <AddVote addVote={this.addVote.bind(this)} />
+            <AddVote />
           </FlexView>
           <FlexView wrap={true} hAlignContent="center">
             <CardFlipper flipCards={this.flipCards.bind(this)} />
