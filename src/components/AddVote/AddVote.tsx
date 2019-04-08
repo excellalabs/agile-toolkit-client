e8c04fc0f2a5bb4682e248eaac375ae3a28abe54
Brendon Caulkins <32872899+brendoncaulkins@users.noreply.github.com>
2019-04-08 19:06:11 -0400

    Add tests (#11)
    * add-tests: Install Enzyme
    
    * add-tests: Install enzyme typescript support, move Jest to dev-dependencies
    
    * add-tests: Add tests for AddVote component

--- src/components/AddVote/AddVote.tsx
+++ src/components/AddVote/AddVote.tsx
@@ -1,29 +1,24 @@
 import * as React from 'react'
 
 interface Props {
-  //classes: Classes
   addVote: Function
 }
 
-interface Classes {
-  fab: string
-}
-
 class AddVote extends React.Component<Props> {
-  updateVoteValue(event) {
+  voteValue: string = ''
+
+  updateVoteValue(event: React.ChangeEvent<HTMLInputElement>) {
     this.voteValue = event.target.value
   }
 
   addVote() {
     this.props.addVote(this.voteValue)
   }
-  voteValue
 
   public render() {
     return (
       <div>
         <input type="text" onChange={event => this.updateVoteValue(event)} />
-        {/* <button onClick={() => this.addVote()} disabled={this.state.hasVoted}>Add Vote</button> */}
         <button onClick={() => this.addVote()}>Add Vote</button>
       </div>
     )
